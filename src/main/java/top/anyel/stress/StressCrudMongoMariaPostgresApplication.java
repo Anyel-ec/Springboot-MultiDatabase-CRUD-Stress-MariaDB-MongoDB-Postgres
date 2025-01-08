2ae0737d2f492aef56f65f95e0b04f0488e39d62
anyel-ec <paulangel826@gmail.com>
2025-01-08 07:42:03 -0500

    chore: start project in springboot 3.3.3

---
+++ src/main/java/top/anyel/stress/StressCrudMongoMariaPostgresApplication.java
@@ -0,0 +1,13 @@
+package top.anyel.stress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StressCrudMongoMariaPostgresApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StressCrudMongoMariaPostgresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/anyel/stress/StressCrudMongoMariaPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package top.anyel.stress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StressCrudMongoMariaPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
